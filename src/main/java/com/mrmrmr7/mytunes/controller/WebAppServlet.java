1e3e587f27c526d54ca4cc58b2abdd5d66531505
mrmr <aliex.s@yandex.by>
2019-02-19 01:26:36 +0300

    Step-by-step web

--- src/main/java/com/mrmrmr7/mytunes/controller/WebAppServlet.java
+++ src/main/java/com/mrmrmr7/mytunes/controller/WebAppServlet.java
@@ -1,7 +1,5 @@
 package com.mrmrmr7.mytunes.controller;
 
-import com.mrmrmr7.mytunes.entity.Command;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,23 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns="/*")
+@WebServlet(urlPatterns="/kek")
 public class WebAppServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doResponce(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doResponce(req, resp);
-    }
-
-    public void doResponce(HttpServletRequest request, HttpServletResponse response) {
-        try {
-            response.getWriter().write("lolkek");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        req.getRequestDispatcher("/jsp/lol.jsp").forward(req,resp);
     }
 }
--- src/main/webapp/web/lol.jsp
+++ src/main/webapp/web/lol.jsp
@@ -1,5 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mrmr
+  Date: 2/19/19
+  Time: 12:15 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-<body>
-<h2>Hello</h2>
-</body>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  lol kek
+  </body>
 </html>
